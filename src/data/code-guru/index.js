9c589c45fbf65862fac59c77fbcd9a0d60e2aa4a
roughatsea <roughatsea@gmail.com>
2026-08-25 00:20:11 -0700

    Publish recognition-first Code Guru field guide

---
+++ src/data/code-guru/index.js
@@ -0,0 +1,12 @@
+import part1 from './part-1.json';
+import part2 from './part-2.json';
+import part3a from './part-3-a.json';
+import part3b from './part-3-b.json';
+const part3 = { ...part3a, chapters: [...part3a.chapters, ...part3b.chapters] };
+import part4 from './part-4.json';
+import part5 from './part-5.json';
+import part6 from './part-6.json';
+import part7 from './part-7.json';
+import part8 from './part-8.json';
+
+export default [part1, part2, part3, part4, part5, part6, part7, part8];
